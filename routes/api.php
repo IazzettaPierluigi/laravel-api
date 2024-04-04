8a497f041159abc8ecfb6133bca3210633d0f0c9
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-04-04 18:35:13 +0200

    api in controller to all projects

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,9 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/test', function () {
-    return response()->json([
-        'name' => 'gianno',
-        'city' => 'rome'
-    ]);
-});
+
+//projectcontroller nella cartella API
+Route::get('/test', [ProjectController::class, 'index']);
