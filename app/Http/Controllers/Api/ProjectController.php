01c1bd26b35f89f00ff878df4cc1c72ba5a5e1f0
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-04-08 15:46:55 +0200

    postcontroller paginate

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,9 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
+        // $projects = Project::all();
+
+        $projects = Project::with('technologies', 'type')->paginate(3);
 
         return response()->json([
             'success' => true,
